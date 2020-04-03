4938f936f9d90ebf707668d9642f70eae99656a1
JohannesNaegele <55059387+JohannesNaegele@users.noreply.github.com>
2020-04-03 16:11:17 +0200

    estimate first try

--- src/estimate.jl
+++ src/estimate.jl
@@ -1,7 +1,38 @@
 cd("/home/johannes/Documents/GitHub/Toy-SMC/src/")
 include("simulate.jl")
 
-function likelihood(Y, N)
+function likelihood(Y::Array{Float64,1}, x0, N=1000)
     n = size(Y)[2]
+    u = Normal()
+    X_normal = zeros(N)
+    X_sample = zeros(N)
+    V = zeros(N)
+    Q = zeros(N)
+    w = Normal()
+    W = zeros(N) #rand(w, N)
     for i in 1:n
+        W[:] = rand(w, N)
+        for j in 1:N
+            if i=1
+                X_normal[j] = 0.5 + 0.3*(x0/(1+x0^2)) + W[j]
+            else
+                X_normal[j] = 0.5 + 0.3*(X_sample[j]/(1+X_sample[j]^2)) + W[j]
+            end
+            V[j] = Y[j] - X_normal[j]
+            Q[j] = pdf(v, V[j])
+        end
+        Q .= Q ./ sum(Q)
+        # in Quantilesfor higher speed
+        for j in 2:N
+            Q[j] += Q[j-1]
+        end
+        for j in 1:N
+            step = rand(u)
+            k = 1
+            while Q[k] < step 
+                k++
+            end
+            X_sample[j] = X_normal[k]
+        end
+    end
 end
--- src/simulate.jl
+++ src/simulate.jl
@@ -1,15 +1,17 @@
 using Distributions
+using Plots
+
 
 function simulate(x0, Y::Array{Float64,2})
     n = size(Y)[2]
-    u = Normal()
-    U = rand(u, n)
+    w = Normal()
+    W = rand(w, n)
     v = TDist(2)
     V = rand(v, n)
-    Y[1,1] = 0.5 + 0.3*(x0/(1+x0^2)) + U[1]
+    Y[1,1] = 0.5 + 0.3*(x0/(1+x0^2)) + W[1]
     Y[2,1] = Y[1,1] + V[1]
     for i in 2:n
-        Y[1,i] = 0.5 + 0.3*(Y[1,i-1]/(1+Y[1,i-1]^2)) + U[i]
+        Y[1,i] = 0.5 + 0.3*(Y[1,i-1]/(1+Y[1,i-1]^2)) + W[i]
         Y[2,i] = Y[1,i] + V[i]
     end
 end
@@ -18,4 +20,7 @@ n = 20
 x0 = 1
 Y = zeros(2,n)
 simulate(x0, Y)
-Y
+Y = Y[1,:]
+plot(Y)
+# v = TDist(2)
+# a = pdf(v, 1)
