88160c7327f50c599d1acb73674a91c2e49ec5ed
JohannesNaegele <55059387+JohannesNaegele@users.noreply.github.com>
2020-04-03 14:36:06 +0200

    Korrektur simulate

---
+++ src/estimate.jl
@@ -0,0 +1,7 @@
+cd("/home/johannes/Documents/GitHub/Toy-SMC/src/")
+include("simulate.jl")
+
+function likelihood(Y, N)
+    n = size(Y)[2]
+    for i in 1:n
+end
--- src/simulate.jl
+++ src/simulate.jl
@@ -3,13 +3,13 @@ using Distributions
 function simulate(x0, Y::Array{Float64,2})
     n = size(Y)[2]
     u = Normal()
-    U = rand(u, n-1)
+    U = rand(u, n)
     v = TDist(2)
     V = rand(v, n)
-    Y[1,1] = x0
+    Y[1,1] = 0.5 + 0.3*(x0/(1+x0^2)) + U[1]
     Y[2,1] = Y[1,1] + V[1]
     for i in 2:n
-        Y[1,i] = 0.5 + 0.3*(Y[1,i-1]/(1+Y[1,i-1]^2)) + U[i-1]
+        Y[1,i] = 0.5 + 0.3*(Y[1,i-1]/(1+Y[1,i-1]^2)) + U[i]
         Y[2,i] = Y[1,i] + V[i]
     end
 end
@@ -18,4 +18,4 @@ n = 20
 x0 = 1
 Y = zeros(2,n)
 simulate(x0, Y)
-println(i)
+Y
