d56a5e47c2f7f12bb86454a6ac4da6e87cabe543
JohannesNaegele <55059387+JohannesNaegele@users.noreply.github.com>
2020-04-06 19:44:54 +0200

    Added parameters in simulate + more explanations

--- src/estimate_forum.jl
+++ src/estimate_forum.jl
@@ -1,6 +1,6 @@
-using Plots
-using BenchmarkTools
 using Distributions
+# using Plots
+# using BenchmarkTools
 # using Optim
 # using ForwardDiff
 # using Traceur
@@ -10,73 +10,79 @@ using Distributions
 
 
 cd("/home/johannes/Documents/GitHub/Toy-SMC/src/")
-include("simulate.jl")
+include("simulate.jl") # include the data-generating function
 
 
-function likelihood(Y::Array{Float64,1}, x0, α, β, δ, σ, N=1000)
+function likelihood(Y::Array{Float64,1}, x0, α, β, δ, σ, N = 1000)
     n = length(Y)
     P = zeros(n)
-    u = Uniform()
     v = TDist(2)
+    w = Normal(0, σ^2) # different than in the paper (squared)
+    u = Uniform() # needed for sampling
     X_normal = zeros(N)
     X_sample = zeros(N)
     V = zeros(N)
     Q = zeros(N)
-    w = Normal(0, σ^2)
     W = zeros(N) 
     for i in 1:n
         W[:] = rand(w, N)
         for j in 1:N
             if i == 1
-                X_normal[j] = α + β*(x0/(1+x0^2)) + W[j]
+                X_normal[j] = α + β * (x0 / (1 + x0^2)) + W[j]
             else
-                X_normal[j] = α + β*(X_sample[j]/(1+X_sample[j]^2)) + W[j]
+                X_normal[j] = α + β * (X_sample[j] / (1 + X_sample[j]^2)) + W[j]
             end
-            V[j] = Y[i] - δ*X_normal[j]
+            V[j] = Y[i] - δ * X_normal[j]
             Q[j] = pdf(v, V[j])
-            # P[i] += X_normal[j]
         end
-        # I divide already here to ensure that P[i] doesn't get too big
-        P[i] = sum(Q)/N
+        # This evaluates the log-likelihood
+        # I divide already here by N to ensure that P[i] doesn't get too big
+        P[i] = sum(Q) / N
+        # I generate quantiles for sampling
         Q .= Q ./ sum(Q)
-
+        # multinomial sampling, I generate a uniform distributed value and divide the Q array in half and look in which part it is (and so on)
         for j in 1:N
             step = rand(u)
             K = 1
-            L = Int(trunc(N/2))
+            L = Int(trunc(N / 2))
             M = N
             k = 1
-            while K != M-1 && k < N
-                if Q[K+L] < step
-                    K = K+L
-                    L = Int(trunc((M-K)/2))
+            while K != M - 1 && k < N
+                if Q[K + L] < step
+                    K = K + L
+                    L = Int(trunc((M - K) / 2))
                 else
-                    M = K+L
-                    L = Int(trunc((M-K)/2))
+                    M = K + L
+                    L = Int(trunc((M - K) / 2))
                     k += 1
                 end
             end  
-            # println(L,K,M)
             if Q[K] < step
                 K += 1
             end    
-            X_sample[j] = X_normal[K]
+            X_sample[j] = X_normal[K] # save the sampled values 
         end
     end
-    return sum(x -> log(x), P)
+    return sum(x->log(x), P)
 end
 
 n = 50 # number of observations
 N = 100000 # number of particles
+α = 0.5; β = 0.3; δ = 1.; σ = 1. # parameters
 x0 = 1 # start value
-Y = zeros(2,n)
+Y = zeros(2, n)
 simulate(x0, Y) # generates hypothetical data
 Y = Y[1,:] # only the observable data
 # plot(Y)
-@btime likely = likelihood(Y, x0, 0.5, 0.3, 1., 1., N)
-likely = likelihood(Y, x0, 1., 0.3, 1., 1., N)
+@time likely = likelihood(Y, x0, α, β, δ, σ, N)
+α = 1. # no real difference
+@time likely = likelihood(Y, x0, α, β, δ, σ, N)
+α = 10. # now it get's finally smaller
+@time likely = likelihood(Y, x0, α, β, δ, σ, N)
 
-# approx(alpha) = likelihood(Y, x0, alpha[1], alpha[2], 1., 1., 40000)
+## additional: Metropolis-Hastings for α and β
+
+# approx(params) = likelihood(Y, x0, params[1], params[2], 1., 1., N)
 # model = DensityModel(approx)
 # p1 = RWMH([Normal(0.55,2), Normal(0.25,2)])
 # @time chain = sample(model, p1, 60000; param_names=["α", "β"], chain_type=Chains)
--- src/simulate.jl
+++ src/simulate.jl
@@ -1,13 +1,13 @@
-function simulate(x0, Y::Array{Float64,2})
+function simulate(x0, Y::Array{Float64,2}, α=0.5, β=0.3, δ=1., σ=1.)
     n = size(Y)[2]
-    w = Normal()
+    w = Normal(0, σ^2)
     W = rand(w, n)
     v = TDist(2)
     V = rand(v, n)
-    Y[1,1] = 0.5 + 0.3*(x0/(1+x0^2)) + W[1]
-    Y[2,1] = Y[1,1] + V[1]
+    Y[1,1] = α + β*(x0/(1+x0^2)) + W[1]
+    Y[2,1] = δ*Y[1,1] + V[1]
     for i in 2:n
-        Y[1,i] = 0.5 + 0.3*(Y[1,i-1]/(1+Y[1,i-1]^2)) + W[i]
+        Y[1,i] = α + β*(Y[1,i-1]/(1+Y[1,i-1]^2)) + W[i]
         Y[2,i] = Y[1,i] + V[i]
     end
 end
