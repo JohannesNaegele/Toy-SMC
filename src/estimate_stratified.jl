b487c9b367e137443c462aa6ffe8fb69f2ccff57
JohannesNaegele <55059387+JohannesNaegele@users.noreply.github.com>
2020-09-14 05:41:37 +0200

    Update estimate_stratified.jl

--- src/estimate_stratified.jl
+++ src/estimate_stratified.jl
@@ -18,7 +18,7 @@ include("simulate.jl") # include the data-generating function
 # addprocs(1)
 # Threads.nthreads()
 
-function likelihood_tipp(Y::Array{Float64,1}, x0, α, β, δ, σ, N = 1000)
+function likelihood(Y::Array{Float64,1}, x0, α, β, δ, σ, N = 1000)
     n = length(Y)
     P = zeros(n)
     v = TDist(2)
@@ -33,7 +33,7 @@ function likelihood_tipp(Y::Array{Float64,1}, x0, α, β, δ, σ, N = 1000)
     W = zeros(N) 
     for i in 1:n
         W[:] = rand(w, N)
-        Threads.@threads for j in 1:N
+        for j in 1:N
             if i == 1
                 X_normal[j] = α + β * (x0 / (1 + x0^2)) + W[j]
             else
@@ -47,25 +47,48 @@ function likelihood_tipp(Y::Array{Float64,1}, x0, α, β, δ, σ, N = 1000)
         P[i] = sum(Q) / N
         # I generate quantiles for sampling
         Q .= Q ./ sum(Q)
-        Q = cumsum(Q) * N
+        Q = cumsum(Q) 
         if true # i == 1
             # We resample with weights
-            v1 = rand(u)*N
-            b[1] = trunc(Q[1]-v1)
-            b0 = trunc(0.0-v1)
-            c[1] = Int(b[1] - b0)
-            # println(c[1])
-            X_sample[1:c[1]] .= X_normal[1]
-            j = c[1]
-            for sim in 2:N 
-                b[1] = Int(trunc(Q[1]-v1))
-                b0 = Int(trunc(0.0-v1))
-                c[1] = Int(b[sim] - b[sim - 1])
-                X_sample[j+1:j+c[sim]] .= X_normal[sim]
-                j += c[sim]
-            end
+            position = (rand(u, N) + [i for i in 0:(N-1)])/N
+            l = 1
+            k = 1
+            while k < N
+                if position[k] < Q[l]
+                    X_sample[k] = X_normal[l]
+                    k += 1
+                else
+                    l+=1
+                end
+            end            
             # x_sample wird automatisch in der nächsten Runde genutzt
         end
     end
     return sum(x->log(x), P)
 end
+
+n = 1000 # number of observations
+N = 10000 # number of particles
+α = 0.5; β = 0.3; δ = 1.; σ = 1.# parameters
+# 0.729, 0.415, 0.538, 0.161 Ergebnis für likelihood
+x0 = 1. # start value
+Y = zeros(2, n)
+simulate(x0, Y, α, β, δ, 1.) # generates hypothetical data
+Y = Y[2,:] # only the observable data
+# plot(Y)
+
+@time likely = likelihood(Y, x0, 0.5, β, 1.0, 1., N)
+@time likely = likelihood_tipp(Y, x0, 0.5, β, 1.0, 1., N)
+@time likely = likelihood(Y, x0, 0.5, β, 1., 0.1, N)
+@time likely = likelihood(Y, x0, 0.729, 0.415, 0.538, 0.161, N)
+@time likely = likelihood_tipp(Y, x0, 0.729, 0.415, 0.538, 0.161, N)
+
+
+approx(params) = likelihood(Y, x0, params[1], params[2], params[3], params[4], N)
+opt(param) = -approx(param)
+parameter = [0.4, 0.4, 1.1, 0.9]
+@time optimum = optimize(opt, parameter, Optim.Options(iterations = 1000))
+Optim.minimizer(optimum)
+
+approx(Optim.minimizer(optimum))
+approx([α, β, δ, σ])
